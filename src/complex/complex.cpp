da8405cb054d1aaa6e14099778fe5aab8740640b
Zamokuhle Kunene <1875438@students.wits.ac.za>
2021-08-24 12:10:39 +0200

    Exercise 3.3

--- src/complex/complex.cpp
+++ src/complex/complex.cpp
@@ -1,27 +1,38 @@
-// complex.cpp
-// Multiplying complex numbers
-
+//Exercise 3.3 Finding the roots of quadratic equations
 #include <iostream>	// contains the definition of cout, endl
 #include <complex>	// contains the complex class definition
-//Exercise 3.1 : I think the commented code "(curly braces)" does not run because it has not been defined
-using namespace std; // cout, endl, complex are all part of this namespace
-// Exercise 3.2 :
-using Complex= std::complex<float>;
+#include <cmath>
 
+using namespace std; // cout, endl, complex are all part of this namespace
+using Complex= std::complex<int>;
+
+void roots(Complex &a, Complex &b, Complex &c)
+{
+        Complex root1=(-b+sqrt(b*b - 4*a*c) ) /2*a ;
+        Complex root2=(-b-sqrt(b*b  - 4*a*c) )  /2*a;
+        
+        cout<<"The first root is: "<<root1.real()<< " + " << root1.imag()<<"j"<<endl; 
+        cout<<"The second root is: "<<root2.real()<< " + " << root2.imag()<<"j"<<endl; 
+ 
+}
 int main()
 {
-	Complex num1 = {2.0, 2.0};  // use auto for type deduction
-	Complex num2 = {4.0, -2.0}; // use uniform initialisation syntax (curly braces)
 
-	Complex answer = num1 * num2; // type deduced for 'answer' is: complex<float>
-
-	cout << "The answer is: " << answer << endl;
-	cout << "Or in a more familiar form: " << answer.real() 
-			<< " + " << answer.imag() << "j" 
-			<< endl	<< endl;
-
-	// answer++;
-
+	Complex a=0;
+    Complex b=0;
+    Complex c=0;
+    //now to ask if user wants to continue
+    char yesNo= 'x';
+    
+   while( yesNo!='q')
+   {
+       cout<<"Enter values for a,b and c to calculate the roots"<<endl;
+        cin>>a>>b>>c;
+        roots(a,b,c);
+        cout<<" Do you wish to calculate roots again?"<<endl;
+        cout<<" To quit enter 'q'. To continue enter any button"<<endl;
+        cin>>yesNo;
+   }
+    
 	return 0;
 }
-
